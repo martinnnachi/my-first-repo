7bb98af2de58be1c6ad0e568125ecc43b7ae57dd
martinnnachi <mnnachi@gmail.com>
2018-04-05 18:44:15 +0100

    added files

---
+++ main.js
@@ -0,0 +1,11 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 6CBC-CDE8
+
+ Directory of C:\Users\nnach\Desktop\my-first-repo
+
+05/04/2018  18:43    <DIR>          .
+05/04/2018  18:43    <DIR>          ..
+05/04/2018  18:43               353 index.html
+05/04/2018  18:43                 0 main.js
+               2 File(s)            353 bytes
+               2 Dir(s)  171,239,436,288 bytes free
